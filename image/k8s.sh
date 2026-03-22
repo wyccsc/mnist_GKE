b5c0f3cf636efff98a692f76249fd94eff2aa8ac
wyccsc <wyc890619@gmail.com>
2026-03-22 15:41:25 +0800

    repo name

--- image/k8s.sh
+++ image/k8s.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker pull wyccsc/KGE_api_service:1.0.0
-docker pull wyccsc/KGE_frontend_service:1.0.0
-docker pull wyccsc/KGE_model_service:1.0.0
+docker pull wyccsc/kge_api_service:1.0.0
+docker pull wyccsc/kge_frontend_service:1.0.0
+docker pull wyccsc/kge_model_service:1.0.0
